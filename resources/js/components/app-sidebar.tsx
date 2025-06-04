0add9cae2674d177fd5e631bcd99b233dddece8e
Ali Raza Marchal <alirazamarchal@hotmail.com>
2025-06-04 11:24:50 +0500

    feat: update sidebar navigation to include Carousel link with appropriate icon

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid, Users } from 'lucide-react';
+import { BookOpen, Folder, Images, LayoutGrid, Users } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -17,6 +17,11 @@ const mainNavItems: NavItem[] = [
         title: 'Management',
         href: '/managments',
         icon: Users,
+    },
+    {
+        title: 'Carousel',
+        href: '/carousels',
+        icon: Images,
     },
 ];
 
