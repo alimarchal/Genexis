dbf91fc914c52db75e480036ffb27ccc1854a2bc
Ali Raza Marchal <alirazamarchal@hotmail.com>
2025-06-05 14:17:31 +0500

    feat: Update contacts migration to allow nullable name and email fields; enhance ContactSeeder for improved data generation

--- database/migrations/2025_06_05_042609_create_contacts_table.php
+++ database/migrations/2025_06_05_042609_create_contacts_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->string('position')->nullable();
             $table->string('department')->nullable();
