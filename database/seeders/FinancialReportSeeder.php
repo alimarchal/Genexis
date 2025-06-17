a017941229530cc06460f960268f2c4fd960cb06
Ayesha Ashraf <akh50381@gmail.com>
2025-06-17 17:13:35 +0500

    add financial report in seeder

--- database/seeders/FinancialReportSeeder.php
+++ database/seeders/FinancialReportSeeder.php
@@ -10,18 +10,63 @@ class FinancialReportSeeder extends Seeder
     public function run(): void
     {
         $financialReports = [
+             [
+                'fiscal_year' => 2013,
+                'first_quarter_report' => 'financial-reports/q1-2013.xls',
+                'half_yearly_report' => 'financial-reports/half-yearly-2013.xls',
+                'third_quarter_report' => 'financial-reports/q3-2013.xls',
+                'annual_report' => 'financial-reports/annual-2013.pdf',
+            ],
+             [
+                'fiscal_year' => 2014,
+                'first_quarter_report' => 'financial-reports/q1-2014.pdf',
+                'half_yearly_report' => 'financial-reports/half-yearly-2014.pdf',
+                'third_quarter_report' => 'financial-reports/q3-2014.pdf',
+                'annual_report' => 'financial-reports/annual-2014.pdf',
+            ], [
+                'fiscal_year' => 2015,
+                'first_quarter_report' => 'financial-reports/q1-2015.xls',
+                'half_yearly_report' => 'financial-reports/half-yearly-2015.pdf',
+                'third_quarter_report' => 'financial-reports/q3-2015.xls',
+                'annual_report' => 'financial-reports/annual-2015.pdf',
+            ], [
+                'fiscal_year' => 2016,
+                'first_quarter_report' => 'financial-reports/q1-2016.pdf',
+                'half_yearly_report' => 'financial-reports/half-yearly-2016.pdf',
+                'third_quarter_report' => 'financial-reports/q3-2016.xls',
+                'annual_report' => 'financial-reports/annual-2016.pdf',
+            ], [
+                'fiscal_year' => 2017,
+                'first_quarter_report' => 'financial-reports/q1-2017.pdf',
+                'half_yearly_report' => 'financial-reports/half-yearly-2017.pdf',
+                'third_quarter_report' => 'financial-reports/q3-2017.pdf',
+                'annual_report' => 'financial-reports/annual-2017.pdf',
+            ],
+             [
+                'fiscal_year' => 2018,
+                'first_quarter_report' => 'financial-reports/q1-2018.pdf',
+                'half_yearly_report' => 'financial-reports/half-yearly-2018.pdf',
+                'third_quarter_report' => 'financial-reports/q3-2018.pdf',
+                'annual_report' => 'financial-reports/annual-2018.pdf',
+            ], [
+                'fiscal_year' => 2019,
+                'first_quarter_report' => 'financial-reports/q1-2019.pdf',
+                'half_yearly_report' => 'financial-reports/half-yearly-2019.pdf',
+                'third_quarter_report' => 'financial-reports/q3-2019.pdf',
+                'annual_report' => 'financial-reports/annual-2019.pdf',
+            ],
             [
                 'fiscal_year' => 2020,
-                'first_quarter_report' => null,
-                'half_yearly_report' => null,
-                'third_quarter_report' => null,
+                'first_quarter_report' => 'financial-reports/q1-2020.pdf',
+                'half_yearly_report' => 'financial-reports/half-yearly-2020.pdf',
+                'third_quarter_report' => 'financial-reports/q3-2020.pdf',
                 'annual_report' => 'financial-reports/annual-2020.pdf',
             ],
             [
                 'fiscal_year' => 2021,
                 'first_quarter_report' => 'financial-reports/q1-2021.pdf',
                 'half_yearly_report' => 'financial-reports/half-yearly-2021.pdf',
-                'third_quarter_report' => null,
+                'third_quarter_report' => 'financial-reports/q3-2021.pdf',
                 'annual_report' => 'financial-reports/annual-2021.pdf',
             ],
             [
@@ -42,8 +87,8 @@ class FinancialReportSeeder extends Seeder
                 'fiscal_year' => 2024,
                 'first_quarter_report' => 'financial-reports/q1-2024.pdf',
                 'half_yearly_report' => 'financial-reports/half-yearly-2024.pdf',
-                'third_quarter_report' => null,
-                'annual_report' => null,
+                'third_quarter_report' => 'financial-reports/q3-2024.pdf',
+                'annual_report' => 'financial-reports/annual-2024.pdf',
             ],
         ];
 
