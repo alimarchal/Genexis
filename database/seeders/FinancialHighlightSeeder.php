749209199172f0179b99fb062a510e06e1880e62
Ayesha Ashraf <akh50381@gmail.com>
2025-06-19 16:17:21 +0500

    Add financial highlights

--- database/seeders/FinancialHighlightSeeder.php
+++ database/seeders/FinancialHighlightSeeder.php
@@ -10,26 +10,26 @@ class FinancialHighlightSeeder extends Seeder
     public function run(): void
     {
         $highlights = [
-            [
-                'fiscal_year' => 2020,
-                'financial_highlights' => 'financial-highlights/highlights-2020.pdf',
-            ],
-            [
-                'fiscal_year' => 2021,
-                'financial_highlights' => 'financial-highlights/highlights-2021.pdf',
-            ],
+            // [
+            //     'fiscal_year' => 2020,
+            //     'financial_highlights' => 'financial-highlights/highlights-2020.pdf',
+            // ],
+            // [
+            //     'fiscal_year' => 2021,
+            //     'financial_highlights' => 'financial-highlights/highlights-2021.pdf',
+            // ],
             [
                 'fiscal_year' => 2022,
-                'financial_highlights' => 'financial-highlights/highlights-2022.pdf',
+                'financial_highlights' => 'financial-highlights/Financial Highlight-2022.pptx',
             ],
-            [
-                'fiscal_year' => 2023,
-                'financial_highlights' => 'financial-highlights/highlights-2023.pdf',
-            ],
-            [
-                'fiscal_year' => 2024,
-                'financial_highlights' => 'financial-highlights/highlights-2024.pdf',
-            ],
+            // [
+            //     'fiscal_year' => 2023,
+            //     'financial_highlights' => 'financial-highlights/highlights-2023.pdf',
+            // ],
+            // [
+            //     'fiscal_year' => 2024,
+            //     'financial_highlights' => 'financial-highlights/highlights-2024.pdf',
+            // ],
         ];
 
         foreach ($highlights as $highlight) {
